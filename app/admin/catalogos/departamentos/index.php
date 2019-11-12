70ec836ea6b06cafe709967e7852347b79ef601e
Jareth Aguilar <jareth.aguilar10@outlook.com>
2019-11-12 16:18:20 -0600

    okay

---
+++ app/admin/catalogos/departamentos/index.php
@@ -0,0 +1,91 @@
+<?php
+require_once '../../../../config/global.php';
+define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo PAGE_TITLE ?></title>
+
+    <?php getTopIncludes(RUTA_INCLUDE ) ?>
+</head>
+
+<body id="page-top">
+
+<?php getNavbar() ?>
+
+<div id="wrapper">
+
+    <?php getSidebar() ?>
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- DataTables Example -->
+            <div class="card mb-3">
+                <div class="card-header">
+                    <i class="fas fa-table"></i>
+                    Catálogo: Departamentos
+                </div>
+                <div class="card-body">
+                    <button class="btn btn-primary mb-3">Nuevo</button>
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+                            <thead>
+                            <tr>
+                                <th>Departamento</th>
+                                <th>Organización</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr>
+                                <td>Recursos Humanos</td>
+                                <td>Organización 1</td>
+                                <td>Editar Eliminar</td>
+                            </tr>
+                            <tr>
+                                <td>Tesorería</td>
+                                <td>Organización 2</td>
+                                <td>Editar Eliminar</td>
+                            </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php getFooter() ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<?php getModalLogout() ?>
+
+<?php getBottomIncudes( RUTA_INCLUDE ) ?>
+</body>
+
+</html>
