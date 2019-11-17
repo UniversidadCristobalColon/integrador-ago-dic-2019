48902487276e93257f7c6460430e190d974f149e
Manuelvillarc <manuelvillarcampos@gmail.com>
2019-11-16 19:53:03 -0600

    Se le ha dado funcionalidad al botón "Nuevo".

--- app/admin/catalogos/decalogos/index.php
+++ app/admin/catalogos/decalogos/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../../../../config/global.php';
+require '../../../../config/db.php';
 
 define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
 ?>
@@ -38,28 +39,106 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
                 </div>
                 <div class="card-body">
 
-                    <button class="btn btn-primary mb-3">Nuevo</button>
+                    <div class="row ml-auto mb-3">
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                            Nuevo
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+
+                                <div class="modal-content">
+                                    <form name="form-nuevo" method="post" action="nuevo.php">
+
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalCenterTitle">Nuevo decálogo</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+
+                                        <div class="modal-body">
+
+                                            <div class="input-group mb-1">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="addon-wrapping">Decálogo</span>
+                                                </div>
+                                                <input type="text" name="nuevodeca" class="form-control" placeholder="Escribe aquí el nombre del nuevo decálogo" aria-label="Username" aria-describedby="addon-wrapping">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                            <input type="submit" class="btn btn-primary" value="Guardar"/>
+                                        </div>
+
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
 
                     <div class="table-responsive">
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                             <tr>
-                                <th>descripcion</th>
-                                <th>creacion</th>
-                                <th>actualizacion</th>
+                                <th>Decálogo</th>
+                                <th>Creación</th>
+                                <th>Actualización</th>
                                 <th>id_escala</th>
                                 <th></th>
                             </tr>
                             </thead>
 
                             <tbody>
-                            <tr>
+
+                            <?php
+                            $sql = "SELECT decalogo, creacion, actualizacion, id_escala FROM decalogos";
+                            $resultado = mysqli_query($conexion,$sql);
+                            if($resultado){
+                                while($fila=mysqli_fetch_assoc($resultado)){
+                                    echo "<tr>";
+                                    echo "<td>$fila[decalogo]</td>";
+                                    echo "<td>$fila[creacion]</td>";
+                                    echo "<td>$fila[actualizacion]</td>";
+                                    echo "<td>$fila[id_escala]</td>";
+                                    echo "<td class='text-center'>";
+                                    echo "<a href='blank'>";
+                                    echo "<i class='fas fa-edit'></i>";
+                                    echo "</a>";
+                                    echo "<a href='blank'>";
+                                    echo "<i class='fa fa-trash'></i>";
+                                    echo "</a>";
+                                    echo "</td>";
+                                    echo "</tr>";
+                                }
+                            }else {
+                                echo "ERROR";
+                            }
+                            ?>
+
+
+                            <!-- <tr>
                                 <td>A</td>
                                 <td>2019/11/01</td>
                                 <td>2019/11/02</td>
                                 <td>D</td>
-                                <td>Editar Eliminar</td>
-                            </tr>
+                                <td class="text-center">
+                                        <a href="blank">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a href="blank">
+                                            <i class="fa fa-trash"></i>
+                                        </a>
+                                </td>
+                            </tr> -->
+
                             </tbody>
 
                         </table>
