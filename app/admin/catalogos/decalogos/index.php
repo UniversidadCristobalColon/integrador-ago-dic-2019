d20bd21fa86cbdf71a248cc3e789e2038fc057fa
Manuelvillarc <manuelvillarcampos@gmail.com>
2019-11-25 17:23:54 -0600

    Se ha cambiado el proceso de registro de un nuevo decálogo, modal eliminado.

--- app/admin/catalogos/decalogos/index.php
+++ app/admin/catalogos/decalogos/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../../../../config/global.php';
 require '../../../../config/db.php';
-
 define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
 ?>
 <!DOCTYPE html>
@@ -16,7 +15,7 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
 
     <title><?php echo PAGE_TITLE ?></title>
 
-    <?php getTopIncludes(RUTA_INCLUDE ) ?>
+    <?php getTopIncludes(RUTA_INCLUDE) ?>
 </head>
 
 <body id="page-top">
@@ -41,57 +40,21 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
 
                     <div class="row ml-auto mb-3">
 
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                        <button onclick="location.href='nuevo.php'"    type="button" class="btn btn-primary">
                             Nuevo
                         </button>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-
-                                <div class="modal-content">
-                                    <form name="form-nuevo" method="post" action="nuevo.php">
-
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalCenterTitle">Nuevo decálogo</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-
-                                        <div class="modal-body">
-
-                                            <div class="input-group mb-1">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" id="addon-wrapping">Decálogo</span>
-                                                </div>
-                                                <input type="text" name="nuevodeca" class="form-control" placeholder="Escribe aquí el nombre del nuevo decálogo" aria-label="Username" aria-describedby="addon-wrapping">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                                            <input type="submit" class="btn btn-primary" value="Guardar"/>
-                                        </div>
-
-                                    </form>
-                                </div>
-
-                            </div>
-                        </div>
 
                     </div>
 
                     <div class="table-responsive">
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
                             <thead>
                             <tr>
                                 <th>Decálogo</th>
                                 <th>Creación</th>
                                 <th>Actualización</th>
-                                <th>id_escala</th>
+                                <th hidden>id_escala</th>
                                 <th></th>
                             </tr>
                             </thead>
@@ -100,44 +63,44 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
 
                             <?php
                             $sql = "SELECT decalogo, creacion, actualizacion, id_escala FROM decalogos";
-                            $resultado = mysqli_query($conexion,$sql);
-                            if($resultado){
-                                while($fila=mysqli_fetch_assoc($resultado)){
+                            $resultado = mysqli_query($conexion, $sql);
+                            if ($resultado) {
+                                while ($fila = mysqli_fetch_assoc($resultado)) {
                                     echo "<tr>";
                                     echo "<td>$fila[decalogo]</td>";
                                     echo "<td>$fila[creacion]</td>";
                                     echo "<td>$fila[actualizacion]</td>";
-                                    echo "<td>$fila[id_escala]</td>";
+                                    echo "<td hidden>$fila[id_escala]</td>";
                                     echo "<td class='text-center'>";
                                     echo "<a href='blank'>";
                                     echo "<i class='fas fa-edit'></i>";
                                     echo "</a>";
-                                    echo "<a href='blank'>";
+                                    echo "<a href='eliminar.php'>";
                                     echo "<i class='fa fa-trash'></i>";
                                     echo "</a>";
                                     echo "</td>";
                                     echo "</tr>";
                                 }
-                            }else {
+                            } else {
                                 echo "ERROR";
                             }
                             ?>
 
-
-                            <!-- <tr>
+                            <!-- Ayuda simple para visualizar la tabla -->
+                            <!--<tr>
                                 <td>A</td>
                                 <td>2019/11/01</td>
                                 <td>2019/11/02</td>
                                 <td>D</td>
-                                <td class="text-center">
-                                        <a href="blank">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <a href="blank">
-                                            <i class="fa fa-trash"></i>
-                                        </a>
+                                <td>
+                                    <a href="#page-top" class="fas fa-edit"></a>
+                                    <form name="form-elim" action="eliminar.php" method="post">
+                                        <input type="submit" value="Borrar">
+                                        <i class="fa fa-trash"></i>
+                                    </form>
                                 </td>
-                            </tr> -->
+                            </tr>-->
+                            <!-- Ayuda simple para visualizar la tabla -->
 
                             </tbody>
 
@@ -146,26 +109,19 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
                 </div>
                 <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
             </div>
-
         </div>
         <!-- /.container-fluid -->
-
         <?php getFooter() ?>
-
     </div>
     <!-- /.content-wrapper -->
-
 </div>
 <!-- /#wrapper -->
-
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
 <?php getModalLogout() ?>
-
-<?php getBottomIncudes( RUTA_INCLUDE ) ?>
+<?php getBottomIncudes(RUTA_INCLUDE) ?>
 </body>
 
 </html>
