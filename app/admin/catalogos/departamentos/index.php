6a6b034ef7561ca65f3853d923be5d6782d6ef62
JarethAguilar <53541039+JarethAguilar@users.noreply.github.com>
2019-12-01 03:21:46 -0600

    Catálogo departamentos semi-listo

--- app/admin/catalogos/departamentos/index.php
+++ app/admin/catalogos/departamentos/index.php
@@ -37,40 +37,68 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
                     Catálogo: Departamentos
                 </div>
                 <div class="card-body">
-                    <button class="btn btn-primary mb-3">Nuevo</button>
+                <button class="btn btn-primary mb-3" onclick="location.href ='nuevo.php'">Agregar</button>
 
                     <div class="table-responsive">
+                    <form id="tableform" action="editar.php" method="post">
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
 
                             <thead>
                             <tr>
                                 <th>Departamento</th>
                                 <th>Organización</th>
+                                <th>Última actualización</th>
+                                <th>Estatus</th>
+                                <th></th>
                             </tr>
                             </thead>
 
                             <tbody>
+                           
+                            <?php foreach ($conexion->query('SELECT * from departamentos') as $row){ ?> 
+                            
                             <tr>
-                                <td>Recursos Humanos</td>
-                                <td>Organización 1</td>
-                                <td>Editar Eliminar</td>
+                                <td><?php echo $row['departamento'] ?></td>
+                                <td><?php echo $row['organizaciones_id']  ?></td>
+                                <td><?php echo $row['ultima_actualizacion'] ?></td>
+                                <td><?php echo $row['estatus'] ?></td>
+                                <td class="text-center">
+                                                        
+                                                        <button type="submit" title="Editar registro"  name="editar" value="<?php  echo $row["id"]; ?>" id="<?php  echo $row["id"]; ?>"  class="btn btn-xs btn-light edit">
+                                                            <i class="fas fa-pencil-alt"></i>
+                                                        </button> 
+                                <?php
+
+                                
+                                
+                                ?>
+                                                        <button type="submit" title="Cambiar estatus" name="cambiar" value="<?php echo $row['id'] ?>" id="<?php  echo $row["id"]; ?>" class="btn btn-xs btn-light change" >
+                                                            <i class="fas fa-exchange-alt"></i>
+                                                        </button>
+                                                    
+                                </td>
                             </tr>
-                            <tr>
-                                <td>Tesorería</td>
-                                <td>Organización 2</td>
-                                <td>Editar Eliminar</td>
-                            </tr>
+<?php
+}
+?>
                             </tbody>
 
                         </table>
+                        </form>
                     </div>
                 </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                <div class="card-footer small text-muted"> Última actualización <?php 
+                
+                foreach ($conexion->query('SELECT actualizacion from organizaciones order by actualizacion desc limit 1') as $fecha){
+                 echo $fecha['actualizacion'];
+                }
+                ?>
+                </div>
             </div>
 
         </div>
         <!-- /.container-fluid -->
-
+                       
         <?php getFooter() ?>
 
     </div>
