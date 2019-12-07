476fb4942a551690323d16d6c19612f5a34a9f02
Foo Bar <53923604+foo1024bar@users.noreply.github.com>
2019-12-07 11:18:49 -0600

    Update login.php

--- app/login.php
+++ app/login.php
@@ -3,6 +3,6 @@
 /*Lógica para autenticar usuarios e iniciar sesión*/
 /*Si contrasena esta vacia no deja iniciar sesion por el md5(str)*/
 
-require '../config/global.php';
+require $_SERVER['DOCUMENT_ROOT'].'/proyecto/config/global.php';
 echo getUrl();
 
