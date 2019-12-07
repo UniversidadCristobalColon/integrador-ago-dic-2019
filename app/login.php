d8f9a156be50a3f3b92403318998fe48f55e134a
Foo Bar <53923604+foo1024bar@users.noreply.github.com>
2019-12-07 11:11:43 -0600

    Update login.php

--- app/login.php
+++ app/login.php
@@ -5,6 +5,7 @@
 
 require '../config/global.php';
 define('PROYECTO', getUrl('../'));
+echo PROYECTO;
 require $_SERVER['DOCUMENT_ROOT'].'/'.PROYECTO.'/config/config.php';
 
 $email = @$_POST['email'];
