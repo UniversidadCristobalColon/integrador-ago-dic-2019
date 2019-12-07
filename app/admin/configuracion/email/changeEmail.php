b1125f944cc1d56285a9ae0764059eb296c93efc
Foo Bar <53923604+foo1024bar@users.noreply.github.com>
2019-12-07 11:41:43 -0600

    Update changeEmail.php

--- app/admin/configuracion/email/changeEmail.php
+++ app/admin/configuracion/email/changeEmail.php
@@ -56,7 +56,7 @@ define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
                     <form>
   <div class="form-group">
     <label for="mailConfigHost">Correo</label>
-    <input type="form-text" class="form-control" id="mailConfigHost" value ="<?php echo $_SESSION['usuario']; ?>" placeholder="Host" readonly>
+    <input type="form-text" class="form-control" id="mailConfigHost" placeholder="<?php echo $_SESSION['usuario']; ?>" readonly disabled>
   </div>
   <div class="form-group">
     <input type="form-text" class="form-control" id="mailConfigPort" value ="" placeholder="Correo Nuevo">
