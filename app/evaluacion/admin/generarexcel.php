b597cb24c8a8b0d4311d00dd69210795b40d79ee
Marlon <marlongc2@gmail.com>
2019-12-09 17:15:08 -0600

    Terminado: Se crean gráficas del excel. pregunta: Cambiaron la tabla de escalas?

--- app/evaluacion/admin/generarexcel.php
+++ app/evaluacion/admin/generarexcel.php
@@ -8,6 +8,14 @@ require __DIR__ . "../../../../vendor/autoload.php";
 $id_evaluado='10';
 $id_periodo='1';
 
+
+use PhpOffice\PhpSpreadsheet\Chart\Chart;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Layout;
+use PhpOffice\PhpSpreadsheet\Chart\Legend;
+use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
+use PhpOffice\PhpSpreadsheet\Chart\Title;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
@@ -126,6 +134,8 @@ while($row2 = mysqli_fetch_array($resultado2)){
 
 
 //TABULADOR
+$dataSeriesLabels=[];
+$dataSeriesValues =[];
 
 $sheet=$documento->createSheet();
 $sheet->setTitle('Tabulador');
@@ -166,20 +176,30 @@ while($row2 = mysqli_fetch_array($resultado2)){
 
     $coordinadaBusqueda='';
     $coordinadaBusqueda2='';
+
+
     if($contusuarios=='0'){
         $sheet->setCellValue('C1', $row2['rol']);
+        $new = array_push($dataSeriesLabels,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Tabulador!$C$1', null, 1)); // 2011)
+        $new = array_push($dataSeriesValues,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Tabulador!$C$2:$C$11', null, 12));
         $coordinadaBusqueda='C';
         $coordinadaBusqueda2='C';
     }elseif ($contusuarios=1){
         $sheet->setCellValue('D1', $row2['rol']);
+        $new = array_push($dataSeriesLabels,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Tabulador!$D$1', null, 1)); // 2011)
+        $new = array_push($dataSeriesValues,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Tabulador!$D$2:$D$11', null, 12));
         $coordinadaBusqueda='D';
         $coordinadaBusqueda2='D';
     }elseif ($contusuarios==2){
         $sheet->setCellValue('E1', $row2['rol']);
+        $new = array_push($dataSeriesLabels,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Tabulador!$E$1', null, 1)); // 2011)
+        $new = array_push($dataSeriesValues,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Tabulador!$E$2:$E$11', null, 12));
         $coordinadaBusqueda='E';
         $coordinadaBusqueda2='E';
     }elseif ($contusuarios==3){
         $sheet->setCellValue('F1', $row2['rol']);
+        $new = array_push($dataSeriesLabels,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Tabulador!$F$1', null, 1)); // 2011)
+        $new = array_push($dataSeriesValues,new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Tabulador!$F$2:$F$11', null, 12));
         $coordinadaBusqueda='F';
         $coordinadaBusqueda2='F';
     }
@@ -208,13 +228,54 @@ while($row2 = mysqli_fetch_array($resultado2)){
     $cont=2;
 }
 
+#
 
+#require __DIR__ . '/../Header.php';
 
+$worksheet = $documento->getSheetByName('Tabulador');
 
+$xAxisTickValues = [
+    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Tabulador!$B$2:$B$11', null, 12), // Jan to Dec
+];
 
+// Build the dataseries
+$series = new DataSeries(
+    DataSeries::TYPE_RADARCHART, // plotType
+    null, // plotGrouping (Radar charts don't have any grouping)
+    range(0, count($dataSeriesValues) - 1), // plotOrder
+    $dataSeriesLabels, // plotLabel
+    $xAxisTickValues, // plotCategory
+    $dataSeriesValues, // plotValues
+    null, // plotDirection
+    null, // smooth line
+    DataSeries::STYLE_MARKER  // plotStyle
+);
+// Set up a layout object for the Pie chart
+$layout = new Layout();
+// Set the series in the plot area
+$plotArea = new PlotArea($layout, [$series]);
+// Set the chart legend
+$legend = new Legend(Legend::POSITION_RIGHT, null, false);
+$title = new Title('Test Radar Chart');
+// Create the chart
+$chart = new Chart(
+    'chart1', // name
+    $title, // title
+    $legend, // legend
+    $plotArea, // plotArea
+    true, // plotVisibleOnly
+    0, // displayBlanksAs
+    null, // xAxisLabel
+    null   // yAxisLabel - Radar charts don't have a Y-Axis
+);
+// Set the position where the chart should appear in the worksheet
+$chart->setTopLeftPosition('A14');
+$chart->setBottomRightPosition('F35');
+
+$worksheet->addChart($chart);
 $documento->removeSheetByIndex(0);
 
-$nombreDelDocumento = "Mi primer archivo.xlsx";
+$nombreDelDocumento = "ReporteDeCompetencias.xlsx";
 
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="' . $nombreDelDocumento . '"');
@@ -223,7 +284,7 @@ header('Cache-Control: max-age=0');
 
 // Redirect output to a client’s web browser (Xlsx)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="ExcelPrueba.xlsx"');
+header('Content-Disposition: attachment;filename="ReportedeCompetencias.xlsx"');
 header('Cache-Control: max-age=0');
 // If you're serving to IE 9, then the following may be needed
 header('Cache-Control: max-age=1');
@@ -235,6 +296,7 @@ header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
 header('Pragma: public'); // HTTP/1.0
 
 $writer = IOFactory::createWriter($documento, 'Xlsx');
+$writer->setIncludeCharts(true);
 $writer->save('php://output');
 exit;
 
