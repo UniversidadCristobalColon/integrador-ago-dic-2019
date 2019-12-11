fcc80c27cc212a7887d94ad79e6b85719c8f1cce
José Rubén Sánchez González <jrsg@outlook.com>
2019-12-11 17:43:47 -0600

    Update init.php

--- app/evaluacion/admin/Cuestionario/init.php
+++ app/evaluacion/admin/Cuestionario/init.php
@@ -6,7 +6,7 @@ $ins = "insert into cuestionarios (cuestionario) values ('')";
 $res = mysqli_query($conexion, $ins);
 if($res){
     $id = mysqli_insert_id($conexion);
-    header("editar.php?id={$id}");
+    header("location: editar.php?id={$id}");
     exit;
 }else{
     echo 'Error al crear el cuestionario';
