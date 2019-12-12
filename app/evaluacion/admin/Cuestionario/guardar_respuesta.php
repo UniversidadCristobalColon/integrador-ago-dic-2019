27739e530ca894fc3360b885a36f9e2fbde11fa5
Armando <arzival.sistemas@gmail.com>
2019-12-11 20:32:38 -0600

    ya casi queda

---
+++ app/evaluacion/admin/Cuestionario/guardar_respuesta.php
@@ -0,0 +1,46 @@
+<?php
+include '../../../../config/db.php';
+$respuestas = $_POST['respuestas'];
+$competencia = $_POST['competencia'];
+$idpregunta = $_POST['idPregunta'];
+$idCuestionario = $_POST['idCuestionario'];
+if (!empty($respuestas)){
+
+        foreach ($respuestas as $id_respuesta){
+            $orden = $_POST["orden_$id_respuesta"];
+            $puntos = $_POST["puntaje_$id_respuesta"];
+            if(!empty($orden) && is_numeric($puntos)){
+                $sqlI = "insert into preguntas_respuestas (id_pregunta, id_respuesta, puntos, orden_respuesta)
+                        values('$idpregunta','$id_respuesta','$puntos','$orden')";
+                $resultado = mysqli_query($conexion, $sqlI);
+            }
+
+    }
+    if (1==1) {
+        $sql = "UPDATE preguntas set id_competencia = '$competencia' where id =  $idpregunta";
+        $resultado = mysqli_query($conexion, $sql);
+
+        if ($conexion->query($sql) === TRUE) {
+            header("location: editar.php?=id_cuestionario=$idCuestionario");
+        } else {
+            echo "Error updating record: " . $conexion->error;
+        }
+
+
+    } else {
+        echo "Error updating record: " . $conexion->error;
+    }
+}else{
+    $sql = "UPDATE preguntas set id_competencia = '$competencia' where id =  $idpregunta";
+    $resultado = mysqli_query($conexion, $sql);
+
+    if ($conexion->query($sql) === TRUE) {
+        header("location: editar.php?=id_cuestionario=$idCuestionario");
+    } else {
+        echo "Error updating record: " . $conexion->error;
+    }
+}
+
+
+
+?>
